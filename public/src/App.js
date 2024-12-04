a7075ab05f9e018ad09361ad28f58e6df7141ac1
NT710 <willmyflightbelate@gmail.com>
2024-12-04 16:29:27 +0100

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FlightDelayPredictor from './components/FlightDelayPredictor';
+
+function App() {
+  return (
+    <FlightDelayPredictor />
+  );
+}
+
+export default App;
