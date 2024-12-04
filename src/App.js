ca3ace013b7c487bf16cd09aa9c0408b7668f41a
NT710 <willmyflightbelate@gmail.com>
2024-12-04 20:56:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import FlightDelayPredictor from './components/FlightDelayPredictor';
-import APITest from './components/APITest';
 
 function App() {
   return (
     <div>
-      <APITest /> {/* Add this temporarily */}
       <FlightDelayPredictor />
     </div>
   );
