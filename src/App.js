fcc2a34610aad97880575c1fcc6f90cfa8a130cc
NT710 <willmyflightbelate@gmail.com>
2024-12-05 15:16:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FlightDelayPredictor from './components/FlightDelayPredictor';
+import FlightDelayPredictor from './components/FlightDelayPredictor.jsx';
 
 function App() {
   return (
