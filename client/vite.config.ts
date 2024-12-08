867db84fb268b0a0379fdb0581b1cd62a718e320
NT710 <willmyflightbelate@gmail.com>
2024-12-08 09:52:51 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      'components': path.resolve(__dirname, './src/components')
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
