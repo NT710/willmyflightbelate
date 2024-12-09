85069f03f3041790dae70fe61b071f9533c701d5
NT710 <willmyflightbelate@gmail.com>
2024-12-09 20:35:46 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,14 +3,21 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: true
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    port: 3000,
+    host: true
   }
 })
