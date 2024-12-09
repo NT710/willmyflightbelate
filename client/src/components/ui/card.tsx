13aa2f097d422542e147e0520f2ffbce44006204
NT710 <willmyflightbelate@gmail.com>
2024-12-09 20:27:00 +0100

    Update card.tsx

--- client/src/components/ui/card.tsx
+++ client/src/components/ui/card.tsx
@@ -1,39 +1,41 @@
 import * as React from "react"
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
 
-export function Card({ className = "", ...props }: CardProps) {
+export const Card: React.FC<CardProps> = ({ className = "", ...props }) => {
   return (
     <div
       className={`rounded-lg border bg-white text-gray-900 shadow-sm ${className}`}
       {...props}
     />
-  )
-}
+  );
+};
 
-export function CardHeader({ className = "", ...props }: CardProps) {
+export const CardHeader: React.FC<CardProps> = ({ className = "", ...props }) => {
   return (
     <div
       className={`flex flex-col space-y-1.5 p-6 ${className}`}
       {...props}
     />
-  )
-}
+  );
+};
 
-export function CardTitle({ className = "", ...props }: React.HTMLAttributes<HTMLHeadingElement>) {
+export const CardTitle: React.FC<React.HTMLAttributes<HTMLHeadingElement>> = ({ className = "", ...props }) => {
   return (
     <h3
       className={`text-2xl font-semibold leading-none tracking-tight ${className}`}
       {...props}
     />
-  )
-}
+  );
+};
 
-export function CardContent({ className = "", ...props }: CardProps) {
+export const CardContent: React.FC<CardProps> = ({ className = "", ...props }) => {
   return (
     <div 
       className={`p-6 pt-0 ${className}`}
       {...props} 
     />
-  )
-}
+  );
+};
