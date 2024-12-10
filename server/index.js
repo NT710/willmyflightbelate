f84b11f424133f7119c1a1deb16456d886bf6bb0
NT710 <willmyflightbelate@gmail.com>
2024-12-10 14:59:23 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cacheService = require('./services/cacheService'); // Ensure this path is correct
 const routes = require('./routes');
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
