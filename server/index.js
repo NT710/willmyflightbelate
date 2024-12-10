ff8bb6995e9c81b071e071f35c534e6f9fedd73f
NT710 <willmyflightbelate@gmail.com>
2024-12-10 15:32:57 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes'); // Your backend API routes
+const routes = require('./routes'); // Import API routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the React app
+// Serve static files from React build directory
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // API Routes
 app.use('/api', routes);
 
-// Catch-all route to serve the React app for all other requests
+// Catch-all to serve React app for other routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
